d4c01438c5686ec66e4ad4058dd6c35c8b1085e9
kazuya sasakura <dropout.college2014@gmail.com>
2021-01-29 16:50:41 +0900

    Initial Commit

---
+++ obi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  obi
+//
+//  Created by 笹倉一也 on 2021/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
